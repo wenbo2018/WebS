af0d4356f43c800128c732b485a58d65a3f50fba
‘wenbo2018’ <wenboing.shen@gmail.com>
2017-05-30 21:32:14 +0800

    fix bug

---
+++ webs-core/src/main/java/com/github/wenbo2018/webs/constants/WebsConstants.java
@@ -0,0 +1,10 @@
+package com.github.wenbo2018.webs.constants;
+
+/**
+ * Created by wenbo.shen on 2017/5/30.
+ */
+public class WebsConstants {
+
+    public static final String DEFAULT_PAGER_DIR="WEB/page/";
+
+}
